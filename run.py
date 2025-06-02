a3b9a8de4b069b405ff66f9ea1e81b6ce910805b
ZBDinev21 <San02694>
2025-06-02 10:32:37 +0300

    commit

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
-from flask import render_template
+from flask import Flask
 
-from app import create_app
-
-app = create_app()
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Secret key for session management
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html'), 404

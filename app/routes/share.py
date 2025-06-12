83a6285126a2ff843f7087fe1ab870259f2e0ee0
ZBDinev21 <San02694>
2025-06-12 11:56:04 +0300

    commit

--- app/routes/share.py
+++
@@ -1,7 +0,0 @@
-@share_bp.route('/consent/<int:id>', methods=['POST'])
-def give_consent(id):
-    response = SurveyResponse.query.get(id)
-    if response.user_id == current_user.id:
-        response.shared = True
-        db.session.commit()
-        flash('Your result is now public.', 'success')
